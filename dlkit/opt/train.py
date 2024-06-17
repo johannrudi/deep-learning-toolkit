b38bfb1e4d7d231d96fa9eb27de11d442b5b3675
Harrison Goldwyn <harrison.goldwyn@nrel.gov>
2024-06-17 17:30:16 -0600

    Added checkpointing to train_epochs

--- dlkit/opt/train.py
+++ dlkit/opt/train.py
@@ -1,9 +1,11 @@
 """Training loops for feed-forward networks.
 """
-
+import os
+import torch
 import logging, timeit
 import numpy as np
 from tqdm import tqdm
+from datetime import datetime
 
 def _dlog_train_epoch_initialize(n_epochs):
     dlog = {}
@@ -20,11 +22,40 @@ def _dlog_train_epoch_update(dlog, epoch_idx, batch_dlog):
 def _dlog_train_epoch_finalize(dlog, time_train):
     dlog['time_train'] = time_train
 
-def train_epochs(n_epochs, net, dataloader, optimizer, loss_fn,
-                 validation_fn=None, device=None, logger=logging.getLogger('train_epochs')):
+def checkpoint(model, filename, epoch, loss, optimizer):
+    torch.save(
+        {
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+            }, 
+        filename
+        )
+    
+def train_epochs(
+    n_epochs, net, dataloader, optimizer, loss_fn,
+    validation_fn=None, device=None, logger=logging.getLogger('train_epochs'),
+    checkpoint_epochs=10, checkpoint_path=None
+    ):
+
     epoch_dlog = _dlog_train_epoch_initialize(n_epochs)
     # <code id="training_loop_over_epochs">
     time_train = timeit.default_timer()
+
+    train_start_time = datetime.now().strftime('d%m%d%y_t%H%M%S')
+
+    ## Prep checkpoint directory if it doesnt exist yet
+    if checkpoint_epochs is not None:
+        # checking if the directory demo_folder  
+        # exist or not. 
+        _check_dir = f"checkpoints/{checkpoint_path}_{train_start_time}"
+        if not os.path.exists(_check_dir): 
+            os.makedirs(_check_dir)
+        _save_checkpoints = True
+    else: 
+        _save_checkpoints = False
+
     for epoch_idx in tqdm(range(n_epochs)):
         # call validation function
         if validation_fn is not None:
@@ -36,6 +67,14 @@ def train_epochs(n_epochs, net, dataloader, optimizer, loss_fn,
         _dlog_train_epoch_update(epoch_dlog, epoch_idx, batch_dlog)
         logger.info("epoch {:6d}, loss_mean {:.6e} std {:.3e}".format(
                 epoch_idx, batch_dlog['loss_mean'], batch_dlog['loss_std']))
+        ## Checkpoint
+        if _save_checkpoints:
+            if epoch_idx % checkpoint_epochs == 0: 
+                filename = f"{_check_dir}/{epoch_idx:02d}-{batch_dlog['loss_mean']:.2f}.pt"
+                checkpoint(
+                    net, filename, epoch=epoch_idx, loss=loss_fn, 
+                    optimizer=optimizer
+                    )
     time_train = timeit.default_timer() - time_train
     # call validation function after training
     if validation_fn is not None:
