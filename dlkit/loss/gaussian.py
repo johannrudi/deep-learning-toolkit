53f71908ef13ea60b96d9f0cc1e412bd7e1b97b8
Harrison Goldwyn <harrison.goldwyn@nrel.gov>
2024-06-18 14:35:01 -0600

    fixed svd inverse algebra and checked dimension math

--- dlkit/loss/gaussian.py
+++ dlkit/loss/gaussian.py
@@ -5,16 +5,18 @@ def _construct_covariance_from_diag_std(std_diag):
 
 def gaussian_loss_fn(outputs, targets, svd_ranks=8, inv_eps=1.0e-5):
     y_mean, y_std_diag = outputs
-    y_data = torch.flatten(targets, 1)
+    y_data = torch.flatten(targets, 1)  # Flatten from dim 1 : -1
     # get covariance matrix
     Cov = _construct_covariance_from_diag_std(y_std_diag)
     # approximate SVD
     U, S, V = torch.svd_lowrank(Cov, q=svd_ranks, niter=2)
     # calculate covariance-weighted inner-product
     y_diff = y_data - y_mean
-    yt_Cinv_y = torch.unsqueeze(y_diff, 1) @ \
-                U @ torch.diag_embed(1.0/(S + inv_eps)) @ torch.transpose(V, 1, 2) @ \
-                torch.unsqueeze(y_diff, 2)
+    yt_Cinv_y = (
+        torch.unsqueeze(y_diff, 1) @ 
+        V @ torch.diag_embed(1.0/(S + inv_eps)) @ torch.transpose(U, 1, 2) @ 
+        torch.unsqueeze(y_diff, 2)
+        )
     # calculate Gaussian loss
     exp_part = yt_Cinv_y.squeeze_([1, 2])
     log_part = torch.sum(torch.log(S), 1)
