57ea2d868f68ce412551d84f750a2ef52786af4e
Johann Rudi <jrudi@vt.edu>
2026-02-28 22:47:41 -0500

    loss: Split gradient penalty terms for Wasserstein critics into a Lipschitz-only penalty and an optimal potential penalty.

--- dlk/loss/wasserstein.py
+++ dlk/loss/wasserstein.py
@@ -1,55 +1,84 @@
 import torch
 
+
 def wasserstein_loss_fn(d_outputs_gen, d_outputs_data):
-    """ Calculates loss corresponding to a Wasserstein distance. """
-    loss_gen  = torch.mean(d_outputs_gen)  if d_outputs_gen  is not None else 0.0
+    """Calculates loss corresponding to a Wasserstein distance."""
+    loss_gen = torch.mean(d_outputs_gen) if d_outputs_gen is not None else 0.0
     loss_data = torch.mean(d_outputs_data) if d_outputs_data is not None else 0.0
-    w_loss = loss_data - loss_gen   # value to be maximized
-    return -w_loss, loss_gen        # value to be minimized
+    w_loss = loss_data - loss_gen  # value to be maximized
+    return -w_loss, loss_gen  # value to be minimized
 
-def wasserstein_reg_fn(d_net, x_gen, x_data, y_data, p=2, c0=1.0, eps=0.0, device=None, dlog=None):
-    """ Computes regularization for network representing Wasserstein distance.
-    This is a penalty term for Lipschitz continuity.
 
-    Author: Deep Ray
-    """
+def gradient_norm_sq(
+    d_net,
+    x_gen,
+    x_data,
+    y_data=None,
+    device=None,
+):
+    """Computes the penalty term for Lipschitz continuity."""
     batch_size, *other_dims = x_data.size()
-    epsilon = torch.rand([batch_size] + [1]*len(other_dims), device=device)
+    epsilon = torch.rand([batch_size] + [1] * len(other_dims), device=device)
     epsilon = epsilon.expand(-1, *other_dims)
     x_hat = epsilon * x_data + (1.0 - epsilon) * x_gen
     x_hat.requires_grad = True
-    y_data.requires_grad = True
-    d_outputs_hat = d_net(x_hat, y_data)
+    if y_data is not None:
+        y_data.requires_grad = True
+        d_outputs_hat = d_net(x_hat, y_data)
+        grad_inputs = (x_hat, y_data)
+    else:
+        d_outputs_hat = d_net(x_hat)
+        grad_inputs = x_hat
     # compute gradient
+    grad_outputs = torch.ones_like(d_outputs_hat, device=device)
     grad = torch.autograd.grad(
         outputs=d_outputs_hat,
-        inputs=(x_hat, y_data),
-        grad_outputs=torch.ones_like(d_outputs_hat, device=device),
+        inputs=grad_inputs,
+        grad_outputs=grad_outputs,
         create_graph=True,
-        retain_graph=True
+        retain_graph=True,
+        only_inputs=True,
     )
-    grad_x, grad_y = grad[0], grad[1]
-    grad_x = grad_x.view(batch_size, -1)
-    grad_y = grad_y.view(batch_size, -1)
     # compute the squared l2-norm of the gradient
-    grad_norm_sq = eps + torch.add(torch.sum(torch.square(grad_x), dim=1),
-                                   torch.sum(torch.square(grad_y), dim=1))
-    # <code id="gradient_penalty_v0">
-    #grad_norm    = torch.sqrt(grad_norm_sq)
-    #grad_penalty = torch.pow(grad_norm - c0, p).mean()
-    # </code>
-    # <code id="gradient_penalty_v1">
-    #grad_norm    = torch.sqrt(grad_norm_sq)
-    #grad_penalty = torch.pow(torch.nn.functional.relu(grad_norm - c0), p).mean()
-    # </code>
-    # <code id="gradient_penalty_v2">
-    grad_norm    = torch.sqrt(grad_norm_sq.detach())  # for logging purposes
-    grad_penalty = torch.nn.functional.relu(grad_norm_sq - c0*c0).mean()
-    # </code>
+    grad_x = grad[0].view(batch_size, -1)
+    grad_x_norm = torch.sum(torch.square(grad_x), dim=1)
+    if y_data is not None:
+        grad_y = grad[1].view(batch_size, -1)
+        grad_y_norm = torch.sum(torch.square(grad_y), dim=1)
+        grad_norm_sq = torch.add(grad_x_norm, grad_y_norm)
+    else:
+        grad_norm_sq = grad_x_norm
+    return grad_norm_sq
+
+
+def gradient_penalty_lip(
+    d_net, x_gen, x_data, y_data, lip=1.0, eps=0.0, device=None, dlog=None
+):
+    """Computes the regularization term for the critic network.
+
+    This penalizes gradients greater `k` to achieve k-Lipschitz continuity.
+    """
+    grad_norm_sq = gradient_norm_sq(d_net, x_gen, x_data, y_data=y_data, device=device)
+    grad_norm = torch.sqrt(grad_norm_sq.detach())  # only for logging purposes
+    grad_penalty = torch.nn.functional.relu(grad_norm_sq + eps - lip * lip).mean()
     # log to dictionary
     if dlog is not None:
         assert isinstance(dlog, dict), type(dlog)
-        dlog['grad_norm'] = grad_norm.detach().mean().item()
-    # return the value of the regularization term
+        dlog["grad_norm"] = grad_norm.detach().mean().item()
     return grad_penalty
 
+
+def gradient_penalty_opt(d_net, x_gen, x_data, y_data, device=None, dlog=None):
+    """Computes the regularization term for the critic network.
+
+    This achieves the optimal Kantorovich potential in the Kantorovich–Rubinstein
+    duality.
+    """
+    grad_norm_sq = gradient_norm_sq(d_net, x_gen, x_data, y_data=y_data, device=device)
+    grad_norm = torch.sqrt(grad_norm_sq)
+    grad_penalty = ((grad_norm - 1.0) ** 2).mean()
+    # log to dictionary
+    if dlog is not None:
+        assert isinstance(dlog, dict), type(dlog)
+        dlog["grad_norm"] = grad_norm.detach().mean().item()
+    return grad_penalty
