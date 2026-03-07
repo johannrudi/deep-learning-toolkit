1f7ef5cab85bc2180816c239b8b041c959111c80
Johann Rudi <jrudi@vt.edu>
2026-03-07 04:32:36 -0500

    Update formatting with black.

--- dlk/loss/gaussian.py
+++ dlk/loss/gaussian.py
@@ -1,7 +1,9 @@
 import torch
 
+
 def _construct_covariance_from_diag_std(std_diag):
-    return torch.diag_embed(std_diag*std_diag)
+    return torch.diag_embed(std_diag * std_diag)
+
 
 def gaussian_loss_fn(outputs, targets, svd_ranks=8, inv_eps=1.0e-5):
     y_mean, y_std_diag = outputs
@@ -12,12 +14,14 @@ def gaussian_loss_fn(outputs, targets, svd_ranks=8, inv_eps=1.0e-5):
     U, S, V = torch.svd_lowrank(Cov, q=svd_ranks, niter=2)
     # calculate covariance-weighted inner-product
     y_diff = y_data - y_mean
-    yt_Cinv_y = torch.unsqueeze(y_diff, 1) @ \
-                V @ torch.diag_embed(1.0/(S + inv_eps)) @ torch.transpose(U, 1, 2) @ \
-                torch.unsqueeze(y_diff, 2)
+    yt_Cinv_y = (
+        torch.unsqueeze(y_diff, 1)
+        @ V
+        @ torch.diag_embed(1.0 / (S + inv_eps))
+        @ torch.transpose(U, 1, 2)
+        @ torch.unsqueeze(y_diff, 2)
+    )
     # calculate Gaussian loss
     exp_part = yt_Cinv_y.squeeze_([1, 2])
     log_part = torch.sum(torch.log(S), 1)
     return torch.mean(exp_part + log_part)
-
-
